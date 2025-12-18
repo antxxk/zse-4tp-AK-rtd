93a751a3b1f637b6351dd4dd24904071e889d124
antxxk <antekplaystation@gmail.com>
2025-12-18 17:52:31 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,15 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('.'))
 
+
+
 project = 'Read the docs'
-author = ' Rafał Zagórowski'
+author = 'Antoni Karkoszka'
+
+
+
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -12,7 +18,9 @@ extensions = [
 ]
 
 templates_path = ['_templates']
+
 exclude_patterns = []
+
 
 html_theme = 'alabaster'
 html_static_path = ['_static']

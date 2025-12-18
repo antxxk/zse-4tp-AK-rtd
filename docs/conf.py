9fbc29fe300be75a5bf0cef611246053411dc9a8
Antoni Karkoszka <antekplaystation@gmail.com>
2025-12-18 15:21:22 +0100

    Dodanie konfiguracji Read the Docs

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+﻿# conf.py dla Sphinx
+project = " Dokumentacja RtD\
+author = \Antoni Karkoszka\
+release = \1.0\
+
+extensions = []
+templates_path = [\_templates\]
+exclude_patterns = []
+
+html_theme = \alabaster\
+html_static_path = [\_static\]
